6b08b15eb9f971b90e6f8620d1c1c38beb46b07d
Vigoluca <lvigotti@gmail.com>
2023-04-07 17:21:02 +0900

    javascript to activate buttons added

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,19 @@
 // This is where it all goes :)
+const sections = document.querySelectorAll('.section');
+const secBtns = document.querySelectorAll('.controls');
+const secBtn = document.querySelectorAll('.control');
+const bodySection = document.querySelectorAll('.main-content');
+
+(function () {
+  [...document.querySelectorAll(".control")].forEach(button => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      // document.querySelector(".active").classList.remove("active");
+      // document.getElementById(button.dataset.id).classList.add("active");
+    })
+  });
+  // document.querySelector(".theme-btn").addEventListener("click", () => {
+  //   document.body.classList.toggle("light-mode");
+  //   })
+})();
