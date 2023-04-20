6683022affc9102511a673bae62e96ba2dca503c
Vigoluca <lvigotti@gmail.com>
2023-04-20 18:34:12 +0900

    fixed js issue

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -16,19 +16,3 @@ const bodySection = document.querySelectorAll('.main-content');
     document.body.classList.toggle("dark");
   })
 })();
-
-window.addEventListener('scroll', reveal);
-
-function reveal() {
-  const reveals = document.querySelectorAll('.reveal');
-  for (let i = 0; i < reveals.length; i++) {
-    let windowheight = window.innerHeight;
-    let revealtop = reveals[i].getBoundingClientRect().top;
-    const revealpoint = 100;
-    if (revealtop < windowheight - revealpoint) {
-      reveals[i].classList.add('active');
-    } else {
-      reveals[i].classList.remove('active');
-    }
-  }
-}
